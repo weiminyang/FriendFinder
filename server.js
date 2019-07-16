336e226079a9fd55425b8ff7a8e7b63265573a91
Weimin Yang <yangwm1983@gmail.com>
2019-07-16 00:57:57 -0700

    google login update

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
 
 
 
